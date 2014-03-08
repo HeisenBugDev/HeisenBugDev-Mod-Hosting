4f8d142cfb64f73693a8b4373f32b1a6b6009715
hunterboerner <hunterboerner@gmail.com>
2014-03-08 15:06:18 -0600

    Rasterizer sizing works. Max height of 1k. Width != height but it is close.

--- app/controllers/rasterizer_controller.rb
+++ app/controllers/rasterizer_controller.rb
@@ -4,30 +4,46 @@ class RasterizerController < ApplicationController
   def create
     require 'mini_magick'
 
+    size = params[:s].to_i
+    size ||= 500
+
+    if size > 1000
+      render :json => {:error => 'Size too big! Max size 1000!'}
+      return
+    end
+    puts "#{size}izesdkfjdkjsfkjkd"
+
+    multiplier = size.to_f / 1000.to_f
+    puts "#{multiplier}sjdkfdkj"
+
+
     top = MiniMagick::Image.open(params[:top])
     side = MiniMagick::Image.open(params[:side])
     front = MiniMagick::Image.open(params[:front])
 
+    x_scale = multiplier * 520
+    y_scale = multiplier * 662
+
     top.combine_options do |c|
-      c.scale('520x602!')
+      c.scale("#{x_scale}x#{602 * multiplier}!")
       c.background('none')
       c.shear('0x30')
       c.rotate('-60')
       c.gravity('center')
       c.scale('100%x85%!')
-      c.crop('2000x602+0+0')
-      c.repage('+0-298')
+      c.crop("#{2000 * multiplier}x#{602 * multiplier}+0+0")
+      c.repage("-1-#{298 * multiplier}")
     end
 
     side.combine_options do |c|
-      c.scale('520x662!')
+      c.scale("#{x_scale}x#{y_scale}!")
       c.background('none')
       c.shear('0x26')
     end
 
     front.combine_options do |c|
       c.brightness_contrast('-20x-18')
-      c.scale('520x662!')
+      c.scale("#{x_scale}x#{y_scale}!")
       c.background('none')
       c.shear('0x-26')
     end
@@ -39,6 +55,7 @@ class RasterizerController < ApplicationController
       c.background('none')
       c.layers('merge')
       c.repage.+
+      c.scale("#{size}x#{size}")
     end
 
     send_data(side.to_blob)
