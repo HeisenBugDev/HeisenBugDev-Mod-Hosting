e036d9351273587d69864d5affc0c409a9255d14
hunterboerner <hunterboerner@gmail.com>
2014-03-11 11:37:46 -0500

    Add in projects model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140311163651) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.string   "repo"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
