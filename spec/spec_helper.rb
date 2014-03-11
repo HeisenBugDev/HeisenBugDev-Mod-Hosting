9c43036268d54b72d84bc6f51ef04ca3ff40cdfc
hunterboerner <hunterboerner@gmail.com>
2014-03-11 16:45:10 -0500

    Stop breaking stuff autorun!!!!!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Coveralls.wear!('rails')
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+# require 'rspec/autorun'
 require 'database_cleaner'
 
 
