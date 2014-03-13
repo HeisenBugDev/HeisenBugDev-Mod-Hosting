8848740fa8cd9973e0773cec3b2c8b366e1f448c
hunterboerner <hunterboerner@gmail.com>
2014-03-13 14:36:53 -0500

    Begin working on a worker :|

---
+++ app/workers/projects_worker.rb
@@ -0,0 +1,20 @@
+class ProjectsWorker
+  include Sidekiq::Worker
+
+  REPO    = 'HeisenBugDev/HeisenBugDev-content'
+  BRANCH  = 'master'
+  FILE    = 'projects.json'
+  GITHUB  = URI("https://raw.github.com/")
+  URN     = "/#{REPO}/#{BRANCH}/#{FILE}"
+
+  def perform
+  end
+
+  def request_file
+    http              = Net::HTTP.new(GITHUB.host, GITHUB.port)
+    http.use_ssl      = true
+    http.verify_mode  = OpenSSL::SSL::VERIFY_NONE
+    request           = Net::HTTP::Get.new(URN)
+    response          = http.request(request)
+  end
+end
