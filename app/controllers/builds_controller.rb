2261b8454bd085b5186d8a47244371a0f5f7d161
hunterboerner <hunterboerner@gmail.com>
2014-03-14 21:04:02 -0500

    Work on the builds controller

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,2 +1,15 @@
 class BuildsController < ApplicationController
+  def create
+    name = params[:project_name]
+    project = Project.find_by_name(name)
+
+    if project.nil?
+      ProjectsWorker.perform_async
+      render :text => "Project was not found. Project database is being"\
+                      "updated. Retry later"
+      return
+    end
+
+    build = Build.new(params.except(:project_name))
+  end
 end
