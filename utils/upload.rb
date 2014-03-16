4c79149152dee3b10aa18592a0e6b01f9f162a6c
hunterboerner <hunterboerner@gmail.com>
2014-03-16 14:30:26 -0500

    Few rubocop cleanups

--- utils/upload.rb
+++ utils/upload.rb
@@ -9,8 +9,12 @@ manifest['artifacts'].each do |artifact|
   artifact['file_data'] = Base64.encode64(content)
 end
 
-req = Net::HTTP::Post.new('/builds', initheader = {'Content-Type' =>'application/json'})
+req = Net::HTTP::Post.new('/builds', 'Content-Type' => 'application/json')
 req.body = manifest.to_json
-response = Net::HTTP.new("localhost", '3000').start {|http| http.request(req) }
+
+response = Net::HTTP.new('localhost', '3000').start do |http|
+  http.request(req)
+end
+
 puts response.body
 puts response
