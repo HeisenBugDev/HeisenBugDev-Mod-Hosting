e41345337ebb6981e91789958064025013b2eda3
hunterboerner <hunterboerner@gmail.com>
2014-03-16 15:36:27 -0500

    hopefully fix now

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -9,22 +9,18 @@ describe ProjectsController do
     it "should increase the Project count by 1" do
       expect { ProjectsWorker.drain }.to change(Project, :count).by(1)
     end
-  end
 
-  describe "when the projects file has new data and there is a refresh" do
-    before do
-      get :refresh_projects
-      ProjectsWorker.drain
-    end
+    context "project exists" do
+      before do
+        get :refresh_projects
+        ProjectsWorker.drain
+      end
 
-    it "should change the description" do
-      urn = '/HeisenBugDev/HeisenBugDev-content/master/projects_data_change_test_env.json'
-      get :refresh_projects, :urn => urn
-      expect { ProjectsWorker.drain }.to change{ Project.find_by_name('QuantumCraft').description }.to('Yet another tech mod')
-    end
-
-    after do
-      Project.delete_all
+      it "should update the description" do
+        urn = '/HeisenBugDev/HeisenBugDev-content/master/projects_data_change_test_env.json'
+        get :refresh_projects, :urn => urn
+        expect { ProjectsWorker.drain }.to change{ Project.find_by_name('QuantumCraft').description }.to('Yet another tech mod')
+      end
     end
   end
 end
