b06004a25962ce4af691bb40c9a50316d0ca634f
hunterboerner <hunterboerner@gmail.com>
2014-03-21 10:27:52 -0500

    fix order

--- app/helpers/builds_helper.rb
+++ app/helpers/builds_helper.rb
@@ -5,9 +5,8 @@ module BuildsHelper
   # @param  amount = 2 [type] Amount of builds to show
   #
   # @return [Array<Build>] Latest builds
-  def get_latest_builds(project, amount = 7)
-    project = Project.find_by_name(project)
-    project.builds.find(:first,
+  def get_latest_builds(project, amount = 3)
+    project.builds.find(:all,
                         :order => 'build_number DESC',
                         :limit => amount)
   end
