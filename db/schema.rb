700010d2d377a4f6fcc4818ee9a63bd9f1244d98
hunterboerner <hunterboerner@gmail.com>
2014-03-22 09:37:20 -0500

    Add omniauth to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321161618) do
+ActiveRecord::Schema.define(version: 20140322143616) do
 
   create_table "artifacts", force: true do |t|
     t.string   "name"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20140321161618) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -15,6 +15,8 @@
 #  last_sign_in_ip        :string(255)
 #  created_at             :datetime
 #  updated_at             :datetime
+#  provider               :string(255)
+#  uid                    :string(255)
 #
 # Indexes
 #
