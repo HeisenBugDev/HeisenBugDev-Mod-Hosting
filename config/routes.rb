6cd375d762cce737fa774d093e35fb3eaf0c2e29
hunterboerner <hunterboerner@gmail.com>
2014-03-24 14:08:49 -0500

    disable non GH login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 HeisenBugDev::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
+    delete 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
+
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 
