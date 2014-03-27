278fd27dc5a96291adba2387cdd376922d0d5b2f
hunterboerner <hunterboerner@gmail.com>
2014-03-27 12:10:57 -0500

    devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,8 @@ class User < ActiveRecord::Base
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable,
+  devise :database_authenticatable,
+  :recoverable, :rememberable, :trackable,
   :omniauthable, :omniauth_providers => [:github]
 
   validates_presence_of :name
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HeisenBugDev::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
     get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_up', :to => 'devise/sessions#new', :as => :new_user_registration
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
