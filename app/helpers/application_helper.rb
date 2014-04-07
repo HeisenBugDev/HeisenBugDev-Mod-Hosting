4b4aae36d67dbe6cdbee478a79eeb6c07aa0bdab
hunterboerner <hunterboerner@gmail.com>
2014-04-07 08:29:49 -0500

    admin link and active tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
  def gravatar_for(user, options = { size: 200 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
+  gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  size = options[:size]
+  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  image_tag(gravatar_url, alt: user.name, class: "gravatar")
+end
 
   # Gets the latest builds in descending order
   # @param  project [String] Name of project
@@ -13,5 +13,13 @@ module ApplicationHelper
   # @return [Array<Build>] Latest builds
   def latest_builds(project, amount = nil)
     project.builds.order('build_number DESC').limit(amount).to_a
+  end
+
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active item' : 'item'
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
   end
 end
