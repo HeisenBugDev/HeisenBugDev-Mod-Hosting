366edf0e4bf5f72da2b5e7b847cc76e7789e0796
hunterboerner <hunterboerner@gmail.com>
2014-04-10 18:26:38 -0500

    use diff-filter=AM and -D instead of using a loop and delete

--- app/workers/wiki/repo_update_worker.rb
+++ app/workers/wiki/repo_update_worker.rb
@@ -13,17 +13,11 @@ class Wiki::RepoUpdateWorker
         `git pull`
         @new_commit = `git rev-parse HEAD`
         commits = "#{@old_commit.delete("\n")} #{@new_commit.delete("\n")}"
-        @files = `git diff --name-only #{commits}`.split("\n")
+        @files = `git diff --name-only --diff-filter=AM #{commits}`.split("\n")
+        @renamed_files = `git diff --name-only --diff-filter=R #{commits}`
+          .split("\n")
         del_file_command = "git diff --diff-filter=D --name-only #{commits}"
         @deleted_files = `#{del_file_command}`.split("\n")
-
-        @files.delete_if do |file|
-          if @deleted_files.include? file
-            true;
-          else
-            false
-          end
-        end
 
         [@files, @deleted_files].each do |array|
           array.map! do |file|
