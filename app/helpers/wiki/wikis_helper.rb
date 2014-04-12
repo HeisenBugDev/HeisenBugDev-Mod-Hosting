3f4eb0307b504e3303d8fcbcc9103c7c4dd3ef6e
hunterboerner <hunterboerner@gmail.com>
2014-04-12 11:35:03 -0500

    remvoe old code

--- app/helpers/wiki/wikis_helper.rb
+++ app/helpers/wiki/wikis_helper.rb
@@ -3,33 +3,6 @@ module Wiki::WikisHelper
     Wiki::Article.where('category_id IS NULL')
   end
 
-  def get_categoriesaaa
-    categories = []
-    get_parent_categories.each do |category|
-      categories << category
-      unless category.subcategories.nil?
-        categories << get_subcategories(category)
-      end
-    end
-    categories
-  end
-
-  def get_subcategories(parent_category)
-    categories = []
-    parent_category.subcategories.each do |sub_category|
-      categories << sub_category
-      unless sub_category.subcategories.nil?
-        categories << get_subcategories(sub_category)
-      end
-    end
-
-    if categories.blank?
-      nil
-    else
-      categories
-    end
-  end
-
   def get_categories
     Wiki::Category.where('parent_id IS NULL')
   end
