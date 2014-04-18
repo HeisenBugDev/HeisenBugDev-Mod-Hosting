2febbfabacb449ad53559d94f8baa65b03370826
hunterboerner <hunterboerner@gmail.com>
2014-04-18 14:05:49 -0500

    dev icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar #{options[:class]}")
   end
 
   # Gets the latest builds in descending order
