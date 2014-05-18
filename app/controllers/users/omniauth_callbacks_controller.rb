8c6e141b073264a1860da8623cfda1d68f365e99
Theron Boerner <hunterboerner@gmail.com>
2014-05-18 11:36:09 -0500

    Line was too long

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_filter :beta_logged_in
 
   def github
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_github_oauth(request.env["omniauth.auth"])
     if @user.save
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      # set_flash_message(:notice, :success, :kind => "GitHub") if is_navigational_format?
+      # this will throw if @user is not activated
+      sign_in_and_redirect @user, :event => :authentication
     else
       redirect_to root_path, :flash => { :warning => "Could not save user." }
     end
