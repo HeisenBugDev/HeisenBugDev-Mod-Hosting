cb8ac5fb49c1b9aee7c2319032316481b865a023
Theron Boerner <hunterboerner@gmail.com>
2014-05-20 06:42:15 -0500

    add @jakerm2002

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,8 @@
 class User < ActiveRecord::Base
   BETA_USERS = %w(hunterboerner forkk sammko k2b6s9j eydamos abrarsyed jadar
     trainerguy22 matalcdev xelitexirish orochimarufan viliml dodolend
-    endershadow HotelCalifornia thejereman13 XixeBombilla).map(&:downcase)
+    endershadow HotelCalifornia thejereman13 XixeBombilla jakerm2002
+    ).map(&:downcase)
   before_validation :beta_user?
   has_and_belongs_to_many :projects
   rolify
