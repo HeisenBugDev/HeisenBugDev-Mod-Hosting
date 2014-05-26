b6f4d2beeae645848e67c55d6627f43b8f8a3394
Theron Boerner <hunterboerner@gmail.com>
2014-05-26 12:34:49 -0500

    Add space there

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ProjectsController do
   let(:user) { FactoryGirl.create(:user) }
   let(:project) { FactoryGirl.create(:project) }
-  let(:wiki) { FactoryGirl.create(:wiki_wiki, :project => project)}
+  let(:wiki) { FactoryGirl.create(:wiki_wiki, :project => project) }
 
   describe 'creating a project without permissions' do
     before do
