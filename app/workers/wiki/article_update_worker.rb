1420c717fa7be43dfd8dcf48d786d9c14b6615bf
Theron Boerner <hunterboerner@gmail.com>
2014-05-28 12:25:45 -0500

    Remove all that commented out code

--- app/workers/wiki/article_update_worker.rb
+++ app/workers/wiki/article_update_worker.rb
@@ -9,26 +9,10 @@ class Wiki::ArticleUpdateWorker
     file_data = File.read(file_params[:file_path])
     wiki = Wiki::Wiki.find(file_params[:wiki_id])
 
-    # base_file = stripped_file.reverse.split('/')[0].reverse
-    # folders = stripped_file.sub(base_file, '').sub('/','').split('/')
-
     title = file_params[:file]
     title.scan(/(\.\w*)/).each do |match|
       title.sub!(match[0], '')
     end
-
-    # if folders[0].nil?
-    #   sliced_folder = ''
-    # else
-    #   sliced_folder = folders[0].dup || ''
-    #   sliced_folder.slice!(0)
-    # end
-
-    # if !folders[0].nil? && folders[0].match(/^(v|b).*/)
-    #   cat_folders = folders.drop(1)
-    # else
-    #   cat_folders = folders
-    # end
 
     category ||= nil
     file_params[:categories].each do |folder|
@@ -56,43 +40,5 @@ class Wiki::ArticleUpdateWorker
       :build_id => version_id, :version_id => build_id, :category_id => cat_id)
     article.update_attributes(:body => file_data.to_s)
     article.save
-
-    # case folders[0]
-    # when /^v.*/
-    #   version = wiki.project.versions.find_by_version(sliced_folder)
-
-    #   return if version.nil?
-    #   if delete
-    #     wiki.articles.find_by_version_id_and_title(version.id, title).destroy
-    #   else
-    #     article = wiki.articles.find_or_initialize_by(:version => version,
-    #       :title => title, :category_id => cat_id)
-
-    #     article.update_attributes(:body => file_data.to_s)
-    #     article.save
-    #   end
-    # when /^b.*/
-    #   build = wiki.project.builds.find_by_build_number(sliced_folder)
-
-    #   return if build.nil?
-    #   if delete
-    #     wiki.articles.find_by_build_id_and_title(build.id, title).destroy
-    #   else
-    #     article = wiki.articles.find_or_initialize_by(:build => build,
-    #       :title => title, :category_id => cat_id)
-    #     article.update_attributes(:body => file_data.to_s)
-    #     article.save
-    #   end
-    # else
-    #   if delete
-    #     wiki.articles
-    #       .find_by_title_and_build_id_and_version_id(title, nil, nil).destroy
-    #   else
-    #     article = wiki.articles.find_or_initialize_by(:title => title,
-    #       :build_id => nil, :version_id => nil, :category_id => cat_id)
-    #     article.update_attributes(:body => file_data.to_s)
-    #     article.save
-    #   end
-    # end
   end
 end
