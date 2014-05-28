91a638a2b131d67e19eb33dd9a695f941e5659a1
Theron Boerner <hunterboerner@gmail.com>
2014-05-28 17:31:55 -0500

    Stupid unneeded tests

--- spec/controllers/documentation_controller_spec.rb
+++ spec/controllers/documentation_controller_spec.rb
@@ -1,19 +1,4 @@
 require 'spec_helper'
 
 describe DocumentationController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
 end
