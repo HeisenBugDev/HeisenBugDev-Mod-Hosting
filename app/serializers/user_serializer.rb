035a102f69f7ab7f8bc31a6395086d33de5044d0
hunterboerner <hunterboerner@gmail.com>
2014-05-29 15:06:19 -0500

    some serializing

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,18 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :email, :name, :authentication_token, :sign_in_count,
+    :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip,
+    :last_sign_in_ip, :created_at, :updated_at
+
+  def filter(keys)
+    unless can? :edit, object
+      keys - [:email, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
+        :current_sign_in_ip, :last_sign_in_ip]
+    end
+
+    unless scope == object
+      keys - [:authentication_token]
+    end
+
+    keys
+  end
 end
--- app/serializers/version_serializer.rb
+++ app/serializers/version_serializer.rb
@@ -1,3 +1,4 @@
 class VersionSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :version, :created_at, :updated_at
+  has_one :project, :embed => :ids
 end
