d7c932af70f913b86ff304907bdb661256e6b4b4
hunterboerner <hunterboerner@gmail.com>
2014-05-29 15:41:20 -0500

    Build serializer

--- app/serializers/build_serializer.rb
+++ app/serializers/build_serializer.rb
@@ -1,3 +1,6 @@
 class BuildSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :build_number, :commit, :minecraft_version, :created_at,
+    :updated_at, :branch, :build_state
+  has_one :project, :embed => :ids
+  has_one :version, :embed => :ids
 end
