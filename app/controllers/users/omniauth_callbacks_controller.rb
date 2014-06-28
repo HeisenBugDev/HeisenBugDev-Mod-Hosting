0267e81f53c522d04b4cc9f294b2d6e8121031d8
Theron Boerner <hunterboerner@gmail.com>
2014-06-28 15:38:16 -0500

    That really should not be there

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_filter :beta_logged_in
 
   def github
-    puts current_user.name
     @user = User.find_for_github_oauth(request.env["omniauth.auth"])
     if @user.save
       # this will throw if @user is not activated
@@ -10,7 +9,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user.save
       params[:omni] = request.env["omniauth.auth"]
       session[:user_id] = @user.id
-      notice = "been signed in!"
       # redirect_to root_path
       # _and_redirect @user, :event => :authentication
     else
