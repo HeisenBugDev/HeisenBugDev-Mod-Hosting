821a83a2138069e060df6efd66d010a1e1f085de
hunterboerner <hunterboerner@gmail.com>
2014-09-01 11:04:10 -0500

    Add pending specs

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -4,4 +4,11 @@ describe ProjectsController do
   let(:user) { FactoryGirl.create(:user) }
   let(:project) { FactoryGirl.create(:project) }
   let(:wiki) { FactoryGirl.create(:wiki_wiki, project: project) }
+
+  pending "Updating"
+  pending "Destroying"
+  pending "Index should not exist"
+  pending "Show"
+  pending "Downloads"
+  pending "Owners"
 end
